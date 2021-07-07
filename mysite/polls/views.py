4fd1d91ae4f483e48edd9d01d897a1be13db1236
peemgay <peempeem2@gmail.com>
2021-07-07 21:30:03 +0700

    tutorial_6

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -7,6 +7,15 @@ from .models import Choice, Question
 from django.utils import timezone
 
 
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
